21c635a017ae3c0528fedd8bd8b1f94ed3aca1b6
marcielleal <marcielmanoel15@gmail.com>
2017-07-03 00:31:35 -0300

    Garantindo uma não duplicidade do pedido de chave

--- instalador.sh
+++ instalador.sh
@@ -1,14 +1,14 @@
 if [ -d "$HOME/.bash_login" ] ; then
-	cp $HOME/.bash_login $HOME/cpbash_login
+	#cp $HOME/.bash_login $HOME/cpbash_login
 	cp validation.py $HOME/validation.py
 fi
 python instalador.py
 python validation.py
 if [ $? -eq 1 ]; then
 	rm $HOME/.bash_login
-	mv $HOME/.cpbash_login $HOME/.bash_login
+	#mv $HOME/.cpbash_login $HOME/.bash_login
    	rm $HOME/.emailInformation
-   	rm validation.py
+   	rm $HOME/validation.py
 else
 	echo "Email cadastrado com sucesso!"
 fi
